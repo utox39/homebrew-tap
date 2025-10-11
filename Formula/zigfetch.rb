1475defb2befdc481e6457a15969083f8fe3517d
utox39 <francescomoccaldi39@gmail.com>
2025-10-11 14:29:22 +0200

    feat: add zigfetch formula

---
+++ Formula/zigfetch.rb
@@ -0,0 +1,22 @@
+class Zigfetch < Formula
+  desc "Zigfetch is a minimal neofetch/fastfetch like system information tool"
+  homepage "https://github.com/utox39/zigfetch"
+  url "https://github.com/utox39/zigfetch/archive/refs/tags/v0.23.0.tar.gz"
+  sha256 "ccf1e418d2005e3bfe12c15aeb7317ac3979859ef8ab564e6acd39edace17372"
+  license "MIT"
+
+  depends_on "zig" => :build
+
+  on_linux do
+    depends_on "pciutils" => :build
+  end
+
+  def install
+    release_mode = :safe
+    system "zig", "build", *std_zig_args
+  end
+
+  test do
+    system "true"
+  end
+end
